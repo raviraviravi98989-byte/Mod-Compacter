5501f7c3d7949660cf1e66595dcd04481960cbbe
raviraviravi98989-byte <raviraviravi98989@gmail.com>
2026-06-03 07:10:39 -0300

    Add files via upload

---
+++ src/main/java/com/blockcompacter/recipe/ModRecipes.java
@@ -0,0 +1,10 @@
+package com.blockcompacter.recipe;
+
+import net.fabricmc.fabric.api.recipe.v1.ingredient.DefaultCustomIngredients;
+
+public class ModRecipes {
+    public static void register() {
+        // The compacting logic is handled by the block entity tick.
+        // This class can be expanded with datapack recipes or custom recipe types.
+    }
+}
